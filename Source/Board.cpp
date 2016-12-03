31b7ae5e465f61e288ed5c166e1d396b8cb62f74
overlordofdoom1 <overlordofdoom12345@gmail.com>
2016-12-03 16:58:42 +0000

    Update Board.cpp

--- Source/Board.cpp
+++ Source/Board.cpp
@@ -3,8 +3,7 @@
 void Board::displayBoard() const
 {
 	for (int row = 0; row < 7; row++) { // Row of the 2D array
-		for (int col = 0; col < 4; col++) // Column of the 2D array
-		{ 
+		for (int col = 0; col < 4; col++) { // Column of the 2D array
 			std::cout << board[row][col]; 
 		}
 		std::cout << "\n";
