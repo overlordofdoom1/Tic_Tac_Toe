930b094109cca8a00fd9266edbc4d4d8113ccbc4
overlordofdoom1 <overlordofdoom12345@gmail.com>
2016-12-11 23:38:54 +0000

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER_H  // Create guard so that Player.h isn't
+#define PLAYER_H  // included more than once in series
+
+#include <iostream>
+#include <string>
+#include "Board.h"
+
+class Player {
+
+private:
+	
+	std::string peice, player;
+	bool winner(Board& board) const; // Checks for winner, pass by refrence (address)
+
+public:
+
+	Player(std::string x);       // Sets up piece, depending on parameter
+	bool move(Board& board);     // Gets input and makes move, pass by refrence (address)
+};
+
+#endif // For def: PLAYER_H
+
